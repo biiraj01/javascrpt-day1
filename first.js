d292e108cad5768e35177fbe80cf466f388c2652
biiraj01 <biraj2003pujari@gmail.com>
2025-05-13 20:30:24 +0545

    js

---
+++ first.js
@@ -0,0 +1,26 @@
+const product =
+{
+    name: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price:270,
+};
+
+console.log(product);
+
+const profile = 
+{
+    username:"biiraj.01",
+    isFollow:false,
+    posts:195,
+    followers:596000 ,
+    following:4,
+    fullName: "Biraj Pujari",
+    job: "Enterprenuer",
+    Quotes: "To educate somenone is the highest privilege",
+};
+
+console.log(profile);
+
+console.log(typeof profile["isFollow"]);
+console.log(typeof product.name);
